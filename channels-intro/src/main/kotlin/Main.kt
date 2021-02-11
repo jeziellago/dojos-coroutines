9b9848d4095aaa80f6aa371b386b0a1e8ed715c4
Jeziel Lago <jeziellago@gmail.com>
2021-02-11 10:06:47 -0300

    Create projeto to channels

---
+++ channels-intro/src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+import kotlinx.coroutines.runBlocking
+
+/**
+ * Kotlin Channels
+ * - Similar BlockingQueue (Java)
+ * - Hot Streams
+ * - Tipos de channels (capacity)
+ * - ReceiveChannel e SendChannel
+ * - Estratégias de BufferOverflow
+ * - Fechamento do stream
+ * */
+
+
+fun main() = runBlocking<Unit> {
+    // BORA!
+}
